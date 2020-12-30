b3d71859e694d308a9f01803f404197ff9bb3b6e
sruthi-2 <71808593+sruthi-2@users.noreply.github.com>
2020-12-30 12:40:41 +0530

    Update linearseach.cpp

--- linearseach.cpp
+++ linearseach.cpp
@@ -1,24 +1,38 @@
 #include <iostream>
 
 int main() {
- 
+    // Write C++ code here
     //linear /sequential search
-    int n,ar[20],find;
-    std::cout<<"enter the number of elements";
+    int n,ar[20],find ,flag=0,no_of_searches=0;
+    std::cout<<"enter the number of elements  :";
     std::cin>>n;
-    std::cout<<"enter the"<<n<<"elements   ";
+    std::cout<<"enter the "<<n<< " elements  : ";
     for(int i=0;i<n;i++)
     {
         std::cin>>ar[i];
     }
-    std::cout<<"enter value to be searched";
+    std::cout<<"enter value to be searched  :";
     std::cin>>find;
     for(int i=0;i<n;i++)
     {
+        ++no_of_searches;
         if(ar[i]==find) 
-              {
-                  std::cout<<"the item"<<ar[i]<<" is found at the "<<i+1<<" lcation";
+              {   
+                  flag=1;
+                 
+                  std::cout<<"the item "<<ar[i]<<" is found at the "<<i+1<<"th location "<<std::endl;
+                  ++flag;
+                
               }
     }
+    if(flag=0)
+    {
+        std::cout<<"item not found in array";
+    }
+    else if(flag>1)
+    {
+        std::cout<<"item is found at multiple locations"<<std::endl;
+    }
+    std::cout<<"we had "<<no_of_searches<<" number of searches"<<std::endl;
     return 0;
 }
